9ffdb242c2111185274cff84af9594c4840baaef
Tom Hey <Tom.Hey@geniusdigital.tv>
2015-03-04 11:43:10 +0000

    Resolves this warning on puppet > 3.4:
    Warning: Puppet::Util::SUIDManager.run_and_capture is deprecated;
    please use Puppet::Util::Execution.execute instead.
    
    Based in upstream commit: 58e20afb531ef982042d2dfe5c3fb12876bf8b0

--- lib/puppet/provider/postgresql_psql/ruby.rb
+++ lib/puppet/provider/postgresql_psql/ruby.rb
@@ -96,10 +96,34 @@ Puppet::Type.type(:postgresql_psql).provide(:ruby) do
 
     if resource[:cwd]
       Dir.chdir resource[:cwd] do
-        Puppet::Util::SUIDManager.run_and_capture(command, resource[:psql_user], resource[:psql_group], { :custom_environment => environment } )
+        if Puppet::PUPPETVERSION.to_f < 3.4
+          Puppet::Util::SUIDManager.run_and_capture(command, resource[:psql_user], resource[:psql_group], { :custom_environment => environment } )
+        else
+          output = Puppet::Util::Execution.execute(command, {
+            :uid                => resource[:psql_user],
+            :gid                => resource[:psql_group],
+            :failonfail         => false,
+            :combine            => true,
+            :override_locale    => true,
+            :custom_environment => environment,
+          })
+          [output, $CHILD_STATUS.dup]
+        end
       end
     else
-      Puppet::Util::SUIDManager.run_and_capture(command, resource[:psql_user], resource[:psql_group], { :custom_environment => environment } )
+      if Puppet::PUPPETVERSION.to_f < 3.4
+        Puppet::Util::SUIDManager.run_and_capture(command, resource[:psql_user], resource[:psql_group], { :custom_environment => environment } )
+      else
+        output = Puppet::Util::Execution.execute(command, {
+          :uid                => resource[:psql_user],
+          :gid                => resource[:psql_group],
+          :failonfail         => false,
+          :combine            => true,
+          :override_locale    => true,
+          :custom_environment => environment,
+        })
+        [output, $CHILD_STATUS.dup]
+      end
     end
   end
 
